8f578a0aa891870c661b4b76bee41383ee45bff3
root <root@CBEN-H867034-LX.dir.svc.accenture.com>
2025-04-26 23:55:17 +0800

    working appservice module with webapp docker with output for link of the webapps

--- othersrvcs/appservices/outputs.tf
+++ othersrvcs/appservices/outputs.tf
@@ -10,3 +10,10 @@ output "subnet_name" {
   value = azurerm_subnet.subnet.name # Updated to match the correct resource name
 }
 
+output "webapp_docker_default_hostname" {
+  value = azurerm_linux_web_app.webapp_docker.default_hostname
+}
+
+output "webapp_nodejs_default_hostname" {
+  value = azurerm_linux_web_app.webapp_nodejs.default_hostname
+}
